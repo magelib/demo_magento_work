68d34c665ac99d000121e2f9915d89a286a9e051
Sandeep Singh <Sandeep.s@dataman.in>
2018-06-11 15:16:23 +0530

    new theme included

--- app/etc/config.php
+++ app/etc/config.php
@@ -6,6 +6,8 @@ return [
     'Magento_Eav' => 1,
     'Magento_Backend' => 1,
     'FME_Banners' => 0,
+    'Hiddentechies_Base' => 1,
+    'Hiddentechies_Bizkick' => 1,
     'Magento_Theme' => 1,
     'Magento_Customer' => 1,
     'Magento_AdminNotification' => 1,
